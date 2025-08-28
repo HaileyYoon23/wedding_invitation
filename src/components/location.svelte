aa183f5151c10f6ab01c0f4484f8ba92671d18e6
hailey.me(나윤서)/kakao <hclove61@gmail.com>
2025-08-28 23:13:38 +0900

    줌 불가

--- src/components/location.svelte
+++ src/components/location.svelte
@@ -33,19 +33,20 @@
 					center: new window.kakao.maps.LatLng(37.515711, 127.105725),
 					level: 4,
 					draggable: false,
+					zoomable: false,
 				};
 				// @ts-ignore
 				const map = new window.kakao.maps.Map(mapContainer, options);
 				
 				// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
-				const mapTypeControl = new window.kakao.maps.MapTypeControl();
+				//const mapTypeControl = new window.kakao.maps.MapTypeControl();
 
 				// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
-				map.addControl(mapTypeControl, window.kakao.maps.ControlPosition.TOPRIGHT);
+				//map.addControl(mapTypeControl, window.kakao.maps.ControlPosition.TOPRIGHT);
 
 				// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
-				const zoomControl = new window.kakao.maps.ZoomControl();
-				map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
+				//const zoomControl = new window.kakao.maps.ZoomControl();
+				//map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
 
 
 				// 마커 추가
